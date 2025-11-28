79444b06b9924d9ee28747295342191f745ad0a5
felipefialayre <felipefialayre@gmail.com>
2025-11-28 16:40:18 -0300

    saque mas funciones

--- source/client_src/client_sender.h
+++ source/client_src/client_sender.h
@@ -11,11 +11,8 @@ public:
     explicit ClientSender(ClientProtocol& protocol, Queue<CliMsgPtr>& senderQueue);
     bool is_listening() const;
     void run() override;
-    bool leerStdinYEncolar();
-    bool parseLine(const std::string& line, std::string& cmd, std::string& param);
 
 private:
     ClientProtocol& protocol;
     Queue<CliMsgPtr>& senderQueue;
-    bool listening{true};
 };
